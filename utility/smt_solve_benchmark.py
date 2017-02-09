7a2bf322d2eef2e3f75e3bed7fb1a6c4a42c1d72
Tony Yang <tianyuyang658@yahoo.com>
2017-02-09 14:28:02 -0800

    Update the benchmark result with more comprehensive information. Update the solver script to make sure multiple assertion results are captured and processed

--- utility/smt_solve_benchmark.py
+++ utility/smt_solve_benchmark.py
@@ -19,7 +19,7 @@ for path, dirs, files in os.walk(translated_benchmark_dir):
         if (smt_file != '' and smt_file != '.DS_Store' and smt_file != 'jSMTLIB.jar'):
             print('Solving {}'.format(smt_file))
             file_path = os.path.join(path, smt_file)
-            expected_status = os.popen("grep -i 'status' " + file_path + " | awk '{print $3}'").read()
+            expected_status = os.popen("grep -i 'status' " + file_path + " | awk '{print $3}'").read().strip()
             expected_result[smt_file] = {'status': expected_status}
             output_path = os.path.join(benchmark_result_dir, '{}.result'.format(smt_file))
 
@@ -27,12 +27,13 @@ for path, dirs, files in os.walk(translated_benchmark_dir):
             os.system('{} --file {} --solver {} > {}'.format(smt_solver, file_path, 1, output_path))
             file_end_time = time.time()
 
-            file_solution = os.popen("grep -i 'x0=' " + output_path + " | awk '{print $0}'").read()
+            # file_solution = os.popen("grep -i 'x0=' " + output_path + " | awk '{print $0}'").read()
+            file_solution = 'na'
             if not file_solution:
                 file_solution = 'unsat'
             actual_result[smt_file] = {
-                'status': os.popen("grep -i 'CPU result' " + output_path + " | awk '{print $4}'").read(),
-                'cputime': os.popen("grep -i 'CPU time' " + output_path + " | awk '{print $4}'").read(),
+                'status': os.popen("grep -i 'CPU result' " + output_path + " | awk '{print $4}'").read().strip().split('\n'),
+                'cputime': os.popen("grep -i 'CPU time' " + output_path + " | awk '{print $4}'").read().strip().split('\n'),
                 'clocktime': file_end_time - file_start_time,
                 'solution': file_solution
             }
@@ -44,19 +45,44 @@ end_time = time.time()
 print('##################')
 print('SMT Solver Summary')
 print('##################')
-print('Total clock time used according to the script: {} seconds\n'.format(end_time - starting_time))
-print('Correct? | SMT Benchmark\nSMT Status | Solver Status | CPU Time | Clock Time | Solution\n')
+print('\nCorrect? | SMT Benchmark\nSMT Status | Solver Status | CPU Time | Clock Time\n')
+
+total_benchmark = 0
+correct_benchmark = 0
+total_cputime = 0.0
 
 for i in range(len(solved_benchmark)):
+    total_benchmark += 1
+    # Processing the actual result status and assertion time since they are in list
+    all_assertion_status = actual_result[solved_benchmark[i]]['status']
+    all_assertion_cputime = actual_result[solved_benchmark[i]]['cputime']
+
+    final_assertion_status = 'sat'
+    final_assertion_cputime = 0.0
+    for j in range(len(all_assertion_status)):
+        if all_assertion_status[j].strip().lower() != 'sat':
+            final_assertion_status = 'unsat'
+    for j in range(len(all_assertion_cputime)):
+        final_assertion_cputime += float(all_assertion_cputime[j].strip())
+
+    total_cputime += final_assertion_cputime
+
     result_correct = 'X' # X = False or not correct
-    if actual_result[solved_benchmark[i]]['status'].strip().lower() == expected_result[solved_benchmark[i]]['status'].strip().lower():
+    if final_assertion_status == expected_result[solved_benchmark[i]]['status'].strip().lower():
         result_correct = 'Y'
+        correct_benchmark += 1
+
     print('{} | {}'.format(result_correct, solved_benchmark[i]))
-    result = '{} | {} | {} | {} | {}'.format(
+    result = '{} | {} | {} | {}'.format(
         expected_result[solved_benchmark[i]]['status'].strip(),
-        actual_result[solved_benchmark[i]]['status'].strip(),
-        actual_result[solved_benchmark[i]]['cputime'].strip(),
-        str(actual_result[solved_benchmark[i]]['clocktime']).strip(),
-        actual_result[solved_benchmark[i]]['solution'].strip()
+        final_assertion_status,
+        str(final_assertion_cputime),
+        str(actual_result[solved_benchmark[i]]['clocktime']).strip()
     )
     print(result)
+
+print('\nSummary')
+print('Total benchmark: {}'.format(total_benchmark))
+print('Correctly solved: {}'.format(correct_benchmark))
+print('Total CPU time: {} seconds'.format(total_cputime))
+print('Total clock time according to the script: {} seconds\n'.format(end_time - starting_time))
